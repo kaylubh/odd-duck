ac169929043d3876723641714f288999939b70f7
Caleb Hemphill <calebhemp@gmail.com>
2023-11-06 18:57:35 -0500

    add function to create image elements

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 
 const productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
 const productObjects = [];
+let randomizedProductObjects = [];
+const voteContainer = document.querySelector('#productImageContainer');
+const productCandidatesPerRound = 3;
 
 // Product constructor function
 function Product(productName) {
@@ -19,4 +22,14 @@ function createProductObjects() {
   }
 }
 
+// render Product image elements
+function renderImgElements () {
+  for (let i = 0; i < productCandidatesPerRound; i++) {
+    const currentImgElement = document.createElement('img');
+    voteContainer.appendChild(currentImgElement);
+  }
+}
+
+// run application
 createProductObjects();
+renderImgElements();
