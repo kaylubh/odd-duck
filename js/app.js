db15bdae1a735e2366ee4a52c86390f61deb9e0c
Caleb Hemphill <calebhemp@gmail.com>
2023-11-07 18:33:52 -0500

    add feature to display results with a chart

--- js/app.js
+++ js/app.js
@@ -20,21 +20,21 @@ function Product(productName) {
   this.views = 0;
   this.votes = 0;
 }
-Product.productObjects = [];
-Product.randomizedProductObjects = [];
+Product.allProducts = [];
+Product.randomizedProducts = [];
 
 // create product objects
 function initProductObjects() {
   for (let i = 0; i < productNames.length; i++) {
     const currentProduct = new Product(productNames[i]);
-    Product.productObjects.push(currentProduct);
+    Product.allProducts.push(currentProduct);
   }
 }
 
 // fix "sweep" product imgSrc file extension
 function fixSweep() {
-  for (let i = 0; i < Product.productObjects.length; i++) {
-    const currentProduct = Product.productObjects[i];
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    const currentProduct = Product.allProducts[i];
     if (currentProduct.productName === 'sweep') {
       currentProduct.imgSrc = 'img/sweep.png';
     }
@@ -43,10 +43,10 @@ function fixSweep() {
 
 // randomize order of the productObjects array with the Fisher-Yates shuffle algorithm via ChatGPT
 function shuffleProductObjects() {
-  Product.randomizedProductObjects = Product.productObjects.slice();
-  for (let i = Product.randomizedProductObjects.length - 1; i > 0; i--) {
+  Product.randomizedProducts = Product.allProducts.slice();
+  for (let i = Product.randomizedProducts.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [Product.randomizedProductObjects[i], Product.randomizedProductObjects[j]] = [Product.randomizedProductObjects[j], Product.randomizedProductObjects[i]];
+    [Product.randomizedProducts[i], Product.randomizedProducts[j]] = [Product.randomizedProducts[j], Product.randomizedProducts[i]];
   }
 }
 
@@ -56,29 +56,29 @@ function renderProductCandidates() {
     endVotingSession();
   }
 
-  if (Product.randomizedProductObjects.length === 0) {
+  if (Product.randomizedProducts.length === 0) {
     shuffleProductObjects();
   }
 
-  firstCandidateInstance = Product.randomizedProductObjects.pop();
+  firstCandidateInstance = Product.randomizedProducts.pop();
   firstCandidateImage.setAttribute('src', firstCandidateInstance.imgSrc);
   firstCandidateImage.setAttribute('alt', firstCandidateInstance.productName);
   firstCandidateInstance.views++;
 
-  if (Product.randomizedProductObjects.length === 0) {
+  if (Product.randomizedProducts.length === 0) {
     shuffleProductObjects();
   }
 
-  secondCandidateInstance = Product.randomizedProductObjects.pop();
+  secondCandidateInstance = Product.randomizedProducts.pop();
   secondCandidateImage.setAttribute('src', secondCandidateInstance.imgSrc);
   secondCandidateImage.setAttribute('alt', secondCandidateInstance.productName);
   secondCandidateInstance.views++;
 
-  if (Product.randomizedProductObjects.length === 0) {
+  if (Product.randomizedProducts.length === 0) {
     shuffleProductObjects();
   }
 
-  thirdCandidateInstance = Product.randomizedProductObjects.pop();
+  thirdCandidateInstance = Product.randomizedProducts.pop();
   thirdCandidateImage.setAttribute('src', thirdCandidateInstance.imgSrc);
   thirdCandidateImage.setAttribute('alt', thirdCandidateInstance.productName);
   thirdCandidateInstance.views++;
@@ -95,13 +95,60 @@ function endVotingSession() {
 
 // render results from voting session
 function renderResults() {
-  for (let i = 0; i < Product.productObjects.length; i++) {
-    const currentProduct = Product.productObjects[i];
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    const currentProduct = Product.allProducts[i];
     const result = `${currentProduct.productName} got ${currentProduct.votes} votes and was shown as an option ${currentProduct.views} times.`;
     const resultListItem = document.createElement('li');
     resultsList.appendChild(resultListItem);
     resultListItem.textContent = result;
   }
+  renderResultsChart();
+}
+
+// display results in a bar chart
+function renderResultsChart() {
+  const productViews = [];
+  const productVotes = [];
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    productViews.push(Product.allProducts[i].views);
+    productVotes.push(Product.allProducts[i].votes);
+  }
+  console.log(productViews);
+  console.log(productVotes);
+
+  // chart settings
+  const data = {
+    labels: productNames,
+    datasets: [
+      {
+        label: 'Views',
+        data: productViews,
+        borderWidth: 1,
+      },
+      {
+        label: 'Votes',
+        data: productVotes,
+        borderWidth: 1
+      }
+    ]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+
+  // render chart
+  const canvas = document.querySelector('#resultsChart');
+  const resultsChart = new Chart(canvas, config);
 }
 
 // add event listeners for votes (clicks) on displayed products
